f6706229298fba033195bc5a7de9ff1dda80af53
VladislavCherepanov <143595420+VladislavCherepanov@users.noreply.github.com>
2024-05-21 18:13:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,101 +2,49 @@ import numpy as np
 import torch
 import matplotlib.pyplot as plt
 
-import params
 import utils 
-import vorticity_model as vm
+import vorticity_model as vort
+import update as upd
+from configs import parameters
 
 from tqdm import tqdm
 
 torch.set_num_threads(12)
 
-H = params.H #domain size
-N = params.N #number of particles
-h0 = H/N #mesh size
+if __name__ == "__main__":
+    H = parameters["domain_size"] 
+    N = parameters["particles_number"] 
+    N_steps =  parameters["steps_number"]
+    T = parameters["total_time"] 
+    dt = T/N_steps #time increment
+    nu = parameters["viscosity_constant"]
+    eps = parameters["boundary_cutoff"] 
+    domain = parameters["domain"]
 
-N_steps = 50 #number of steps
-T = 1.0 #total time
-dt = T/N_steps #time increment
+    #initialise lattice points for the domain
+    lattice_points, h_0 = utils.create_lattice()
 
-nu = 0.01 #viscosity constant
-eps = params.eps #boundary cutoff parameter
+    #indicators of hitting the boundary
+    if domain == "channel": tau = (lattice_points[:,1] > 0).float()*(lattice_points[:,1] < H).float()
+    elif domain == "half-plane": tau = (lattice_points[:,1] > 0).float()
 
+    #initialize the particles at lattice points
+    X = torch.clone(lattice_points)
 
-#initialise lattice points for the domain
-lattice_points = utils.create_lattice()
+    #print the initial vorticity
+    vort.print_vorticity(omega = utils.initial_vorticity)
 
-#indicators of hitting the boundary
-tau = torch.tensor((lattice_points[:,1] > 0).float())
+    #tensors to store the cumulative external vorticity from perturbation and force
+    if domain == "channel": 
+        cutoff_values_lower, cutoff_values_upper = utils.cutoff(X)
+        boundary_vorticity_lower, boundary_vorticity_upper = utils.zeta(X, utils.initial_vorticity)
+        ext_vort_pert = nu/(eps**2)*(boundary_vorticity_lower*cutoff_values_lower + boundary_vorticity_upper*cutoff_values_upper)*dt
+    elif domain == "half-plane": 
+        ext_vort_pert = nu/(eps**2)*utils.zeta(X, utils.initial_vorticity)*utils.cutoff(X)*dt
+    ext_vort_force = utils.external_force(X)*dt
 
-#initialise the particles at lattice points
-X = torch.clone(lattice_points)
+    #print the initial stream
+    vort.print_stream(omega = utils.initial_vorticity)
 
-#compute initial vorticity
-omega_0 = utils.initial_vorticity(X)
-
-#print the initial vorticity
-fig = plt.figure(figsize = (12,10)) 
-ax = plt.axes(projection='3d') 
-surf = ax.plot_surface(torch.reshape(lattice_points[:,0], (2*N+1, N+1)).numpy(), 
-                       torch.reshape(lattice_points[:,1], (2*N+1, N+1)).numpy(), 
-                       torch.reshape(omega_0, (2*N+1, N+1)).numpy(), cmap = plt.cm.coolwarm) 
-plt.show() 
-plt.close()
-
-#object storing the current vorticity model
-omega = None
-parameters = None
-
-#tensors to store the cumulative external vorticity from perturbation and force
-ext_vort_pert = nu/(eps**2)*utils.theta(utils.initial_vorticity, X)*utils.cutoff(X)*dt
-ext_vort_force = utils.external_force(X, 0)*dt
-
-
-
-for t in tqdm(range(N_steps)):
-    X_temp = torch.clone(X)
-
-    #initial vorticity plus time integral of the external vorticity
-    time_integral = omega_0*tau + ext_vort_pert + ext_vort_force
-
-    #compute the vorticity and find its values (at lattice points)
-    omega, omega_values = vm.launch(time_integral, X, parameters)
-
-    #values to update the particles positions
-    X_upd = u(X, omega_values)*dt + np.sqrt(2*nu)*np.sqrt(dt)*torch.randn_like(X)
-
-    #indicators for particles that just crossed the boundary
-    tau_upd = torch.tensor((X_temp[:,1] > 0).float()*(X_temp[:,1] + X_upd[:,1] <= 0).float())
-
-    #update the indicators for particles that hit the boundary
-    tau *= (1.0 - tau_upd)
-
-    #update the particles positions and set the crossing particles
-    #to the corrsponding point at the boundary 
-    X = X_temp + X_upd*torch.tensordot(tau, torch.tensor([1.0, 1.0]), dims=0) - torch.tensordot(X_temp[:,1]/X_upd[:,1], torch.tensor([1.0, 1.0]), dims=0)*X_upd*torch.tensordot(tau_upd, torch.tensor([1.0, 1.0]), dims=0)
-
-    #update the terms with the external vorticity setting them to zero
-    #for particles that just crossed the boundary
-    ext_vort_pert = ext_vort_pert*(1.0 - tau_upd) + nu/(eps**2)*utils.theta(omega, X)*utils.cutoff(X)*dt
-    ext_vort_force = ext_vort_force*(1.0 - tau_upd) + utils.external_force(X, (t+1)*dt)*dt
-
-    #write print function
-    vel = u(lattice_points, omega_values)
-    a1 = vel[:,0]
-    b1 = vel[:,1]
-    fig = plt.figure(figsize=(12,12))
-    plt.contourf(torch.reshape(lattice_points[:,0], (2*N+1, N+1)).numpy(), torch.reshape(lattice_points[:,1], (2*N+1, N+1)).numpy(), 
-                 torch.reshape(omega_values, (2*N+1, N+1)).numpy(),cmap="coolwarm",levels=100)
-    plt.colorbar()
-    st3=plt.streamplot(torch.reshape(lattice_points[:,0], (2*N+1, N+1)).numpy().T, torch.reshape(lattice_points[:,1], (2*N+1, N+1)).numpy().T, 
-                       torch.reshape(a1, (2*N+1, N+1)).numpy().T, torch.reshape(b1, (2*N+1, N+1)).numpy().T,
-                       color=np.sqrt(torch.reshape(a1, (2*N+1, N+1)).numpy().T**2+torch.reshape(b1, (2*N+1, N+1)).numpy().T**2),
-                       cmap ='viridis',arrowsize=0.8,density=1.5)
-    plt.colorbar(st3.lines)
-    plt.show()
-    plt.close
-
-
-    plt.scatter(X[:,0].numpy(), X[:,1].numpy())
-    plt.show()
-    plt.close()
+    for t in tqdm(range(N_steps)):
+        upd.step_update(X, tau, ext_vort_pert, ext_vort_force)
